6e094311be70dae62e8f28be69b00187890784ae
DiegoAZamoraD <dazamorad99@gmail.com>
2022-11-10 19:30:55 -0500

    Se agrega el front para login y register, asi como sus funcionalidades, para que el usuario se pueda registrar y loggear

--- ecommerce/client/src/GlobalState.js
+++ ecommerce/client/src/GlobalState.js
@@ -1,5 +1,6 @@
-import React, {createContext, useState} from "react";
+import React, {createContext, useEffect, useState} from "react";
 import ProductsAPI from './api/ProductsAPI'
+import axios from "axios";
 
 
 export const GlobalState = createContext()
@@ -7,9 +8,20 @@ export const GlobalState = createContext()
 export const DataProvider = ({children})=>{
     const [token, setToken] = useState(false)
 
+    const refreshToken = async () =>{
+        const res = await axios.get('/user/refresh_token')
 
+        setToken(res.data.accesstoken)
+    }
 
-    ProductsAPI()
+    useEffect(()=>{   
+        const firstLogin = localStorage.getItem('firstLogin')
+        if(firstLogin)refreshToken()
+        
+         
+    },[])
+
+    // ProductsAPI()
     const state = {
         token: [token, setToken],
         productsAPI: ProductsAPI()
